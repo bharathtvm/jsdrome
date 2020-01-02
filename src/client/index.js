50a30e82a773db9960da2ee7c36f344fb78c363c
bharath <thebharathpr@gmail.com>
2020-01-02 23:23:57 +0530

    chore: react

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Person from './person';
+
+const name = {
+  firstname: "Bharath",
+  lastname: "Padmanabhan",
+}
 
 class App extends Component {
   render() {
-    return <h1>Hello, india</h1>;
+    return (
+      <div>
+        <h1>JS Drome batch 1</h1>
+        <Person name={name}/>
+      </div>
+    )
   }
 }
 
